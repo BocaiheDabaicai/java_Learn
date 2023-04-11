236294b63d6daecd0ecc3d9b6454c629192e641b
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-04-11 16:58:29 +0800

    两个案例 信息输出 爱心输出

---
+++ Chapter_01/Love.java
@@ -0,0 +1,23 @@
+public class Love {
+    public static void main(String[] args){
+        System.out.print("    *                    *\n");
+        System.out.print("  *     *  I Love Java *     *\n");
+        System.out.print("    *                      *\n");
+        System.out.print("      *                  *\n");
+        System.out.print("        *              *\n");
+        System.out.print("          *          *\n");
+        System.out.print("            *      *\n");
+        System.out.print("              *  *\n");
+
+        System.out.print("\t\t*\t\t\t\t\t\t*\n");
+        System.out.print("\t*\t\t* I Love Java\t*\t\t*\n");
+        System.out.print("\t  *\t\t\t\t\t\t\t  *\n");
+        System.out.print("\t\t*\t\t\t\t\t\t*\n");
+        System.out.print("\t\t  *\t\t\t\t\t  *\n");
+        System.out.print("\t\t\t*\t\t\t\t*\n");
+        System.out.print("\t\t\t  *\t\t\t  *\n");
+        System.out.print("\t\t\t\t*\t\t*\n");
+        System.out.print("\t\t\t\t  *\t  *\n");
+        System.out.print("\t\t\t\t\t*\n");
+    }
+}
---
+++ Chapter_01/Person.java
@@ -0,0 +1,7 @@
+public class Person {
+        public static void main(String[] args){
+            System.out.println("姓名:康师傅\n");
+            System.out.println("性别:男");
+            System.out.println("家庭住址:北京程序员聚集地:回龙观");
+        }
+}
